70c6c3958ff13509ab95619d336bc07247756cc8
Reazul Islam Reaz <reazulislam1487@gmail.com>
2025-10-15 21:50:34 +0600

    add comment

--- src/routes/coursesRoutes.js
+++ src/routes/coursesRoutes.js
@@ -7,7 +7,7 @@ import {
 } from "../controllers/courseControllers.js";
 import verifyToken from "../middlewares/authMiddlewares.js";
 const router = express.Router();
-
+// Course Api
 router.post("/create", createCourse);
 router.put("/update", updateCourse);
 router.get("/all", verifyToken, getCourses);
